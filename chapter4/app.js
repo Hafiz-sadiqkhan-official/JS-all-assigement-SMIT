d654b56d99efb8ed761310a6f04e50ae7c9e836e
Hafiz-sadiqkhan-official <princessfarzana1999@gmail.com>
2025-09-25 00:42:42 +0500

    Add files via upload

---
+++ chapter4/app.js
@@ -0,0 +1,37 @@
+// cahpter no 3 
+
+// question no 1 answer
+
+var a,b,c ;
+
+// // question no 2 answer
+
+
+// // legall name 
+
+// sadiqkhan
+// mera_name
+// $dollar
+// number123
+// _kuchbhi
+
+// illegal names
+
+// my name 
+// var 123
+// document.
+// -operator
+// 123sadiq
+
+// question no 3 answer
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain <b>letters</b>, <b>numbers</b>, <b>$</b> and <b>_</b>.<br>");
+document.write("For example: <b>$my_1stVariable</b><br><br>");
+
+document.write("Variables must begin with a <b>letter</b>, <b>$</b> or <b>_</b>.<br>");
+document.write("For example: <b>$name</b>, <b>_name</b> or <b>name</b><br><br>");
+
+document.write("Variable names are case <b>sensitive</b>.<br><br>");
+
+document.write("Variable names should not be JS <b>keywords</b>.");
