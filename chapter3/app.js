333eb9de2aec5b48365475a68f4be05d13247f79
Hafiz-sadiqkhan-official <princessfarzana1999@gmail.com>
2025-09-25 00:42:24 +0500

    Add files via upload

---
+++ chapter3/app.js
@@ -0,0 +1,42 @@
+
+// chapter 3 
+
+// quesstion no 1 answer
+
+var age;
+age = 18;
+alert("i am " + age + " year old")
+
+var age = 18;
+alert("I am " + age + " years old");
+
+
+// quesstion no 2 answer
+
+
+
+
+// quesstion no 3 answer
+
+var birthyear = 2005;
+
+document.write( "my birth year is " + birthyear + "<br>");
+document.write("Data type of my declared variable is number " +"<br>" );
+
+var birthYear = 2005;
+
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is number " );
+
+
+// question no 4 answer
+
+ "<br>"
+
+var name = "John Doe";
+var product = "T-shirt(s)";
+var quantity = 5;
+
+document.write(name + " ordered " + quantity + "" + product + " on XYZ Clothing store");
+
+
