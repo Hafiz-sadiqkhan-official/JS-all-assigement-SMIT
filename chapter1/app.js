5ab22cb0d9c7561f7b3a2cec14f7c36f8b597fcb
Hafiz-sadiqkhan-official <princessfarzana1999@gmail.com>
2025-09-25 00:41:29 +0500

    Add files via upload

---
+++ chapter1/app.js
@@ -0,0 +1,31 @@
+// // chapter 1 
+
+// // question no 1 answer
+
+alert( "welcome to my website");
+
+
+// // question no 2 answer
+document.write("welcome to karachhi");
+
+// // question no 3 answer
+
+alert("assalamualikum \n walikumsalam");
+
+// // question no 4 answer
+
+alert("message 1 ");
+alert("message 2");
+alert("message 3");
+
+
+// question no 5 answer
+
+console.log("yeh console answe hy ");
+
+// question no 6 answer
+alert("mere website ka link ")
+
+// question no 7 answer 
+// a code chala a me 
+
