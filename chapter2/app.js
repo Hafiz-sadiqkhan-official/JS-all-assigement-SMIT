6617f337cbece0f9989e4d22631a716b0064951c
Hafiz-sadiqkhan-official <princessfarzana1999@gmail.com>
2025-09-25 00:41:59 +0500

    Add files via upload

---
+++ chapter2/app.js
@@ -0,0 +1,59 @@
+// chapter  2 
+
+
+// quesstion no 1 answer
+
+var username;
+
+// quesstion no 2 answer
+
+var myName = "sadiqkhan";
+
+// quesstion no 3 answer
+
+var message = "hello world";
+
+alert(message);
+
+// quesstion no 4 answer
+
+var name = "sadiq";     
+var age = "17";
+var job = "student"
+var gender = "male"
+
+alert(name);
+alert(age);
+alert(job);
+alert(gender);
+
+
+// quesstion no 5 answer
+
+var kaam;
+kaam="pizza\nplzz\npiz\npip\npi\np"
+alert(kaam)
+
+// quesstion no 6 answer
+
+var email;
+email= "cyberatttack44@gmail.com"
+alert( "myemail address is" + email)
+
+
+// quesstion no 7 answer
+
+var book;
+book="A smarter way to learn JavaScript"
+alert("i am trying to learn from the book" + book)
+
+// quesstion no 8 answer
+
+
+document.write(" yeh I can write HTNL content through javascript")
+
+// quesstion no 9 answer
+
+var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design);
+document.write('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”')
